ac69ce4f83cfb109b2aa1014dc1b51c021e2faff
Ryan.Nguyen <Ryan.Nguyen@avepoint.com>
2023-03-23 09:07:43 +0700

    a

---
+++ Code/NodeJS Other/Final/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+// # Bảo mật đăng nhập / # Dùng Express session
+
+app.use(session({
+  resave: true, 
+  saveUninitialized: true, 
+  secret: 'somesecret', // nên lưu trong env
+  cookie: { maxAge: 6000 } // session hết hạn sau 60000 giây. Sau khoảng thời gian đó connectid k dùng được nx và phải set lại. Ở đây trong browser vẫn lưu connectid nhưng k dùng làm gì đc, lấy ra sẽ là null
+}));
+// Mặc định nó dùng httponly. Có nhiều thông số khác có thể set.
+
+var server = app.listen(3000, "127.0.0.1", function () {
+  var host = server.address().address
+  var port = server.address().portS
+  console.log("Example app listening at http://%s:%s", host, port)
+});
+
+app.get('/set_session', (req, res) => {
+  req.session.User = {
+    website: 'anonystick.com',
+    type: 'blog javascript',
+    like: '4550'
+  }
+  return res.status(200).json({status: 'success'})
+})
+
+app.get('/get_session', (req, res) => {
+  if(req.session.User){
+    return res.status(200).json({status: 'success', session: req.session.User})
+  }
+  return res.status(200).json({status: 'error', session: 'No session'})
+})
+
+app.get('/destroy_session', (req, res) => {
+  req.session.destroy(function(err) {
+    return res.status(200).json({status: 'success', session: 'cannot access session here'})
+  })
+})
